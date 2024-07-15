90f8707e6e469b27f1f70457ff11a3e0de3a78eb
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-15 14:45:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+	if(s.length === 0){
+		return ""
+	}
+	let indexOfFirstSpace = s.indexOf(" ");
+	if(indexOfFirstSpace === -1){
+		return s;
+	}
+	return s.substr(0, indexOfFirstSpace)
 }
 
 // Do not change the code below
